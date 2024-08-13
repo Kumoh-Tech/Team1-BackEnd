e962cf7d38cf7f5a2cd7490e6da276ae843d0b72
jungbk0808 <jungbk0808@gmail.com>
2024-08-13 14:22:07 +0900

    🎉 init project

---
+++ src/main/java/com/club_board/club_board_server/ClubBoardServerApplication.java
@@ -0,0 +1,13 @@
+package com.club_board.club_board_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubBoardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubBoardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/club_board/club_board_server/ClubBoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club_board.club_board_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubBoardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
