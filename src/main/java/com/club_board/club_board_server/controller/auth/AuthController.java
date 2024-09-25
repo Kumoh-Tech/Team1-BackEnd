f872708c108e55de5d5d22f76fa4be36462e86e7
Juhyeok <kyoung1678@naver.com>
2024-09-25 15:50:39 +0900

    :sparkles:feat: 로그인 컨트롤러 구현

--- src/main/java/com/club_board/club_board_server/controller/auth/AuthController.java
+++ src/main/java/com/club_board/club_board_server/controller/auth/AuthController.java
@@ -9,8 +9,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,12 +30,4 @@ public class AuthController {
         authService.forgotPassword(resetPasswordRequest);
         return ResponseEntity.ok(ResponseUtil.createSuccessResponse("임시 비밀번호 생성 완료"));
     }
-    @PreAuthorize("@tokenProvider.hasRoleInClub(#clubId,'일반회원')")
-    @PostMapping("/club/{clubId}/manage")
-    public ResponseEntity<ResponseBody<String>> manageClub(@PathVariable Long clubId) {
-        // 동아리 관리 로직
-        return ResponseEntity.ok(ResponseUtil.createSuccessResponse("동아리 관리 화면 접근 성공"));
-    }
-
-
 }
