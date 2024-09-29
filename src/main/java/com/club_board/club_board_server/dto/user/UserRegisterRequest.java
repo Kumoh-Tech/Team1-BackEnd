343d3def27e7a4423b211f206d7f72a320675f9b
Juhyeok <kyoung1678@naver.com>
2024-09-29 16:15:24 +0900

    :sparkles:feat: 프론트엔드와 정규식 일치하게 수정

--- src/main/java/com/club_board/club_board_server/dto/user/UserRegisterRequest.java
+++ src/main/java/com/club_board/club_board_server/dto/user/UserRegisterRequest.java
@@ -10,16 +10,18 @@ import lombok.Getter;
 public class UserRegisterRequest {
 
     @NotNull
+    @Pattern(message="올바르지 않는 아이디 형식입니다.",
+            regexp = "/^A-Za-z0-9*@kumoh\\.ac\\.kr$/")
     private String username;
 
     @NotNull
     @Pattern(message = "비밀번호는 최소 10자 이상~20자 이하, 영문 대문자, 소문자, 특수문자를 포함해야합니다.",
-           regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@!#$%^&*()_+])[A-Za-z\\d@!#$%^&*()_+]{10,20}$")
+           regexp = " /^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[\\W_])[A-Za-z\\d\\W_]{10,20}$/")
     private String password;
 
     @NotNull
     @Pattern(message = "비밀번호는 최소 10자 이상~20자 이하, 영문 대문자, 소문자, 특수문자를 포함해야합니다.",
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@!#$%^&*()_+])[A-Za-z\\d@!#$%^&*()_+]{10,20}$")
+            regexp = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@!#$%^&*()_+])[A-Za-z\\d@!#$%^&*()_+]{10,20}$/")
     private String confirmPassword;
 
     @NotBlank
@@ -29,12 +31,14 @@ public class UserRegisterRequest {
     private String department;
 
     @NotBlank
+    @Pattern(regexp = "/^\\d{8}$/")
     private String student_id;
 
     @NotNull
     private int grade;
 
     @NotBlank
+    @Pattern(regexp = "/^\\d{3}-\\d{4}-\\d{4}$/")
     private String phone_number;
 }
 
