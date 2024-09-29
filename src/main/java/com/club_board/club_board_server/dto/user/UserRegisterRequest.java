ac37bf81b106d3c05ff462fc83179da677b1b53a
Juhyeok <kyoung1678@naver.com>
2024-09-29 16:18:52 +0900

    :sparkles:feat: 프론트엔드와 정규식 일치하게 수정

--- src/main/java/com/club_board/club_board_server/dto/user/UserRegisterRequest.java
+++ src/main/java/com/club_board/club_board_server/dto/user/UserRegisterRequest.java
@@ -5,23 +5,22 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
-
 @Getter
 public class UserRegisterRequest {
 
     @NotNull
-    @Pattern(message="올바르지 않는 아이디 형식입니다.",
-            regexp = "/^A-Za-z0-9*@kumoh\\.ac\\.kr$/")
+    @Pattern(message = "올바르지 않은 아이디 형식입니다.",
+            regexp = "^[A-Za-z0-9._%+-]+@kumoh\\.ac\\.kr$")
     private String username;
 
     @NotNull
-    @Pattern(message = "비밀번호는 최소 10자 이상~20자 이하, 영문 대문자, 소문자, 특수문자를 포함해야합니다.",
-           regexp = " /^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[\\W_])[A-Za-z\\d\\W_]{10,20}$/")
+    @Pattern(message = "비밀번호는 최소 10자 이상~20자 이하, 영문 대문자, 소문자, 특수문자를 포함해야 합니다.",
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@!#$%^&*()_+])[A-Za-z\\d@!#$%^&*()_+]{10,20}$")
     private String password;
 
     @NotNull
-    @Pattern(message = "비밀번호는 최소 10자 이상~20자 이하, 영문 대문자, 소문자, 특수문자를 포함해야합니다.",
-            regexp = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@!#$%^&*()_+])[A-Za-z\\d@!#$%^&*()_+]{10,20}$/")
+    @Pattern(message = "비밀번호는 최소 10자 이상~20자 이하, 영문 대문자, 소문자, 특수문자를 포함해야 합니다.",
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@!#$%^&*()_+])[A-Za-z\\d@!#$%^&*()_+]{10,20}$")
     private String confirmPassword;
 
     @NotBlank
@@ -31,15 +30,15 @@ public class UserRegisterRequest {
     private String department;
 
     @NotBlank
-    @Pattern(regexp = "/^\\d{8}$/")
+    @Pattern(message = "학번은 8자리 숫자여야 합니다.",
+            regexp = "^\\d{8}$")
     private String student_id;
 
     @NotNull
     private int grade;
 
     @NotBlank
-    @Pattern(regexp = "/^\\d{3}-\\d{4}-\\d{4}$/")
+    @Pattern(message = "전화번호 형식이 올바르지 않습니다.",
+            regexp = "^\\d{3}-\\d{4}-\\d{4}$")
     private String phone_number;
 }
-
-
