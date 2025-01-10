8de36fecd744efd5e391cc84b17eae80c1057731
Juhyeok <kyoung1678@naver.com>
2025-01-10 16:38:22 +0900

    :sparkles:feat: 예외 타입 수정

--- src/main/java/com/club_board/club_board_server/response/exception/ExceptionType.java
+++ src/main/java/com/club_board/club_board_server/response/exception/ExceptionType.java
@@ -23,6 +23,7 @@ public enum ExceptionType {
     EXPIRED_EMAIL_CODE(BAD_REQUEST,"U007","만료된 인증번호입니다."),
     EMAIL_NOT_VERIFIED(UNAUTHORIZED,"U008","이메일 인증이 완료되지 않았습니다."),
     INVALID_DEPARTMENT(BAD_REQUEST,"U009","해당하는 학과가 존재하지 않습니다."),
+    USER_OVERDUE(UNAUTHORIZED,"U010","연체된 유저입니다."),
 
     //Auth
     INVALID_LOGIN(HttpStatus.UNAUTHORIZED,"A001","아이디 또는 비밀번호가 일치하지 않습니다."),
