37bc5a7eaa2c9a0b294a46216fe7e9d78663af1e
Juhye0k <kyoung1678@naver.com>
2025-01-17 13:12:39 +0900

    enh: BookRepository 책 id 조회 비관적 락 적용

--- src/main/java/com/club_board/club_board_server/repository/book/BookRepository.java
+++ src/main/java/com/club_board/club_board_server/repository/book/BookRepository.java
@@ -1,9 +1,19 @@
 package com.club_board.club_board_server.repository.book;
 
 import com.club_board.club_board_server.domain.book.Book;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT b FROM Book b WHERE b.id=:bookId")
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Book> findBookWithPessimisticLock(@Param("bookId") Long bookId);
 }
