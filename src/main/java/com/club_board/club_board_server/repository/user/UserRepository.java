fca9b6c04f786c504babc0f19fef99cb049b6ca1
Juhye0k <kyoung1678@naver.com>
2025-01-22 16:47:01 +0900

    refactor: UserRepository findById Optional 변경

--- src/main/java/com/club_board/club_board_server/repository/user/UserRepository.java
+++ src/main/java/com/club_board/club_board_server/repository/user/UserRepository.java
@@ -6,5 +6,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
-    User findById(Long id);
+    Optional<User> findById(Long id);
 }
