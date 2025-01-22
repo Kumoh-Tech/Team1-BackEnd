be02daeb6ce30889a37553efcc2df7e60b4210a0
jungbk0808 <jungbk0808@gmail.com>
2025-01-22 16:01:26 +0900

    :sparkles:feat: AUTH_WHITELIST 최신화

--- src/main/java/com/club_board/club_board_server/config/security/WebSecurityConfig.java
+++ src/main/java/com/club_board/club_board_server/config/security/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig{
     private final TokenProvider tokenProvider;
     private final CustomUserDetailsService customUserDetailsService;
     private static final String[] AUTH_WHITELIST = {
-            "/register/**","/login/**","/post/**","/comment/**","/admin/**","/myPage/**","/board/**","/club/**", "/book/**"
+            "/register/**","/login/**","/post/**","/comment/**","/admin/**","/myPage/**","/board/**","/club/**", "/books/**", "/book/admin/**"
     };
 
     @Bean
