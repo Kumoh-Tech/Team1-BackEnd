46a89fb5a342ae46121b089c2d8c596226192349
Juhye0k <kyoung1678@naver.com>
2025-02-12 20:34:01 +0900

    refactor: 반환값 success boolean으로 수정

--- src/main/java/com/club_board/club_board_server/response/ResponseBody.java
+++ src/main/java/com/club_board/club_board_server/response/ResponseBody.java
@@ -7,12 +7,10 @@ import lombok.Setter;
 @Setter
 @Getter
 @JsonTypeInfo( // 다형성 처리, 어떤 하위 클래스로 역직렬화 할지
-        use=JsonTypeInfo.Id.CUSTOM, // NAME을 사용하여 각 하위 클래스에 대해 이름을 지정
+        use=JsonTypeInfo.Id.NAME, // NAME을 사용하여 각 하위 클래스에 대해 이름을 지정
         include = JsonTypeInfo.As.EXISTING_PROPERTY, // 타입 정보를 JSON 속성으로 포함시킨다.
         property = "success",
         visible = true)   // property이름 : success
-
-@JsonTypeIdResolver(BooleanTypeIdResolver.class)
 public sealed  abstract class ResponseBody<T> permits SuccessResponseBody, FailedResponseBody{
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String code;
