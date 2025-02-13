f662b6c4d7465e8cfa3ca6e293b60d0f7b75044b
juhyeok <96466824+Juhye0k@users.noreply.github.com>
2025-02-13 20:28:30 +0900

    Update AuthService.java
    테스트용으로 쿠키 Secure설정 false

--- src/main/java/com/club_board/club_board_server/service/auth/AuthService.java
+++ src/main/java/com/club_board/club_board_server/service/auth/AuthService.java
@@ -175,7 +175,7 @@ public class AuthService {
         String cookieName="refresh-token";
         Cookie cookie=new Cookie(cookieName, refreshToken);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge(60);
         return cookie;
