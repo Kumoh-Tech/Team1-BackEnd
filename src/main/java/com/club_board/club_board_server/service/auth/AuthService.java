21ba33803dd10865fb6acd5144a7a6bda0e8697e
juhyeok <96466824+Juhye0k@users.noreply.github.com>
2025-02-13 20:18:12 +0900

    Update AuthService.java
    테스트용으로 쿠키 시간 1분

--- src/main/java/com/club_board/club_board_server/service/auth/AuthService.java
+++ src/main/java/com/club_board/club_board_server/service/auth/AuthService.java
@@ -177,7 +177,7 @@ public class AuthService {
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
         cookie.setPath("/");
-        cookie.setMaxAge(60*60*24*7);
+        cookie.setMaxAge(60);
         return cookie;
     }
 
