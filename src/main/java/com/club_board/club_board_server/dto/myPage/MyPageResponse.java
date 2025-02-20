ef5b95218b9b85fe17385fa419f65387ca048df7
Juhye0k <kyoung1678@naver.com>
2025-02-20 13:55:06 +0900

    feat: 마이페이지 dto

--- src/main/java/com/club_board/club_board_server/dto/myPage/MyPageResponse.java
+++ src/main/java/com/club_board/club_board_server/dto/myPage/MyPageResponse.java
@@ -4,18 +4,30 @@ import lombok.Getter;
 
 @Getter
 public class MyPageResponse {
+    //TODO: 역할을 DTO로 전달 OR 토큰으로 전달?
+    private String username;
     private String name;
     private String department;
     private String studentId;
     private int grade;
     private String phoneNumber;
+    private boolean departmentPublic;
+    private boolean studentIdPublic;
+    private boolean gradePublic;
+    private boolean phonePublic;
 
     @Builder
-    public MyPageResponse(String name, String department, String studentId, int grade, String phoneNumber) {
+    public MyPageResponse(String username,String name, String department, String studentId, int grade, String phoneNumber
+    , boolean departmentPublic, boolean studentIdPublic, boolean gradePublic, boolean phonePublic) {
+        this.username = username;
         this.name=name;
         this.department=department;
         this.studentId=studentId;
         this.grade=grade;
         this.phoneNumber=phoneNumber;
+        this.departmentPublic=departmentPublic;
+        this.studentIdPublic=studentIdPublic;
+        this.gradePublic=gradePublic;
+        this.phonePublic=phonePublic;
     }
 }
--- src/main/java/com/club_board/club_board_server/dto/myPage/UpdateMyPageRequest.java
+++ src/main/java/com/club_board/club_board_server/dto/myPage/UpdateMyPageRequest.java
@@ -1,14 +1,40 @@
 package com.club_board.club_board_server.dto.myPage;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class UpdateMyPageRequest {
-    private String name;
+
+    @NotBlank
+    private String prePassword;
+
+    @NotBlank
+    @Pattern(message = "비밀번호는 최소 10자 이상~20자 이하, 영문 대문자, 소문자, 특수문자를 포함해야 합니다.",
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@!#$%^&*()_+])[A-Za-z\\d@!#$%^&*()_+]{10,20}$")
+    private String newPassword;
+
+    @NotBlank
     private String department;
-    private String studentId;
+
+    @NotBlank
     private int grade;
+
+    @NotBlank
+    @Pattern(message = "전화번호 형식이 올바르지 않습니다.",
+            regexp = "^\\d{3}-\\d{4}-\\d{4}$")
     private String phoneNumber;
-    private Boolean is_department_public;
-    private Boolean is_grade_public;
-    private Boolean is_phone_public;
+
+    @NotBlank
+    private Boolean departmentPublic;
+
+    @NotBlank
+    private Boolean studentIdPublic;
+
+    @NotBlank
+    private Boolean gradePublic;
+
+    @NotBlank
+    private Boolean phonePublic;
 }
