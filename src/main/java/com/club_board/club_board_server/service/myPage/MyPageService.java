67f3cfb9ab872b2605c4787ee03260d3b7a01912
Juhye0k <kyoung1678@naver.com>
2025-02-20 13:54:17 +0900

    feat: 마이페이지 서비스 개발

--- src/main/java/com/club_board/club_board_server/service/myPage/MyPageService.java
+++ src/main/java/com/club_board/club_board_server/service/myPage/MyPageService.java
@@ -2,12 +2,14 @@ package com.club_board.club_board_server.service.myPage;
 import com.club_board.club_board_server.domain.user.User;
 import com.club_board.club_board_server.dto.myPage.MyPageResponse;
 import com.club_board.club_board_server.dto.myPage.UpdateMyPageRequest;
+import com.club_board.club_board_server.dto.myPage.UpdateUserCommand;
 import com.club_board.club_board_server.repository.user.UserRepository;
 import com.club_board.club_board_server.response.exception.BusinessException;
 import com.club_board.club_board_server.response.exception.ExceptionType;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -15,14 +17,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MyPageService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     public MyPageResponse getMyPage(Long userId){
         User user=userRepository.findById(userId)
                 .orElseThrow(()->new BusinessException(ExceptionType.USER_NOT_FOUND));
         return MyPageResponse.builder()
+                .username(user.getUsername())
                 .name(user.getName())
                 .department(user.getDepartment())
                 .studentId(user.getStudent_id())
                 .grade(user.getGrade())
+                .phoneNumber(user.getPhoneNumber())
+                .departmentPublic(user.is_department_public())
+                .studentIdPublic(user.is_student_public())
+                .phonePublic(user.is_phone_public())
+                .gradePublic(user.is_grade_public())
                 .build();
     }
 
@@ -30,11 +39,21 @@ public class MyPageService {
     public void updateMyPage(Long userId, UpdateMyPageRequest updateMyPageRequest){
         User user=userRepository.findById(userId)
                 .orElseThrow(()->new BusinessException(ExceptionType.USER_NOT_FOUND));
-        user.updateUserInfo(
-                updateMyPageRequest.getName(),
-                updateMyPageRequest.getDepartment(),
-                updateMyPageRequest.getStudentId(),
-                updateMyPageRequest.getGrade(),
-                updateMyPageRequest.getPhoneNumber());
+        if(!passwordEncoder.matches(updateMyPageRequest.getPrePassword(), user.getPassword())){
+            throw new BusinessException(ExceptionType.NOT_CORRECT_PASSWORD);
+        }
+        String encodePassword=passwordEncoder.encode(updateMyPageRequest.getNewPassword());
+        UpdateUserCommand command = UpdateUserCommand.builder()
+                .password(encodePassword)
+                .department(updateMyPageRequest.getDepartment())
+                .grade(updateMyPageRequest.getGrade())
+                .phoneNumber(updateMyPageRequest.getPhoneNumber())
+                .departmentPublic(updateMyPageRequest.getDepartmentPublic())
+                .gradePublic(updateMyPageRequest.getGradePublic())
+                .studentIdPublic(updateMyPageRequest.getStudentIdPublic())
+                .phonePublic(updateMyPageRequest.getPhonePublic())
+                .build();
+        user.updateUserInfo(command);
+        userRepository.save(user);
     }
 }
