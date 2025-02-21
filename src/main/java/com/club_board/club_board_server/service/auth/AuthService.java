49aa06e6937103d87c9641f64a59081a306633b4
Juhye0k <kyoung1678@naver.com>
2025-02-21 21:27:55 +0900

    refactor: 테스트

--- src/main/java/com/club_board/club_board_server/service/auth/AuthService.java
+++ src/main/java/com/club_board/club_board_server/service/auth/AuthService.java
@@ -174,7 +174,7 @@ public class AuthService {
                 .httpOnly(true)
                 .secure(true)          // 운영 환경에서는 HTTPS 사용 시 true, 개발 환경에서는 false로 설정 가능
                 .path("/")
-                .maxAge(60 )
+                .maxAge(60*60)
                 .sameSite("None")      // SameSite를 None으로 설정
                 .build();
         response.addHeader("Set-Cookie", cookie.toString());
