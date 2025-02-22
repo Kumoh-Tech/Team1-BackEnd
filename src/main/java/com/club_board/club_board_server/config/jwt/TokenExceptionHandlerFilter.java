9236565c759b080938cf71d25548c65ff16112f0
Juhye0k <kyoung1678@naver.com>
2025-02-22 19:59:59 +0900

    refactor: 테스트용 토큰 로그

--- src/main/java/com/club_board/club_board_server/config/jwt/TokenExceptionHandlerFilter.java
+++ src/main/java/com/club_board/club_board_server/config/jwt/TokenExceptionHandlerFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,6 +17,7 @@ import java.io.PrintWriter;
 
 
 @Component
+@Slf4j
 public class TokenExceptionHandlerFilter extends OncePerRequestFilter { // OncePerRequestFilter : 한 요청당 필터가 딱 한 번만 실행되도록 보장하는 추상 클래스
 
     @Override
@@ -35,16 +37,16 @@ public class TokenExceptionHandlerFilter extends OncePerRequestFilter { // OnceP
         response.setCharacterEncoding("UTF-8"); // HttpServletResponse: ISO-8859-1 인코딩 사용하기 때문에 한글 출력을 위해 UTF-8 설정
         ResponseBody<Void> body= ResponseUtil.createFailureResponse(exceptionType);
         writeErrorResponse(response,body);
-        if(!response.isCommitted())
-            response.flushBuffer();
+
     }
     private void writeErrorResponse(HttpServletResponse response, ResponseBody<Void> body) throws IOException{
         ObjectMapper objectMapper = new ObjectMapper(); // Jackson ObjectMapper 인스턴스 생성
         String json = objectMapper.writeValueAsString(body);  // ResponseBody 객체를 JSON 문자열로 직렬화
-
+        log.error("응답 에러 JSON: {}", json);
         try (PrintWriter writer = response.getWriter()) { // PrintWriter 자원을 안전하게 닫기 위해 try-with-resource 사용
             writer.write(json);
             writer.flush();
+            response.flushBuffer();
         }
     }
 }
