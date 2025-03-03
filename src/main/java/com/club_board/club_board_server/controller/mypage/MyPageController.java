37831204ab7f7eaa088e030da5af1578149ea09b
Juhye0k <kyoung1678@naver.com>
2025-03-03 13:56:43 +0900

    refactor: 마이페이지 디렉토리 구분

--- src/main/java/com/club_board/club_board_server/controller/mypage/MyPageController.java
+++ src/main/java/com/club_board/club_board_server/controller/mypage/MyPageController.java
@@ -1,6 +1,6 @@
 package com.club_board.club_board_server.controller.mypage;
-import com.club_board.club_board_server.dto.myPage.MyPageResponse;
-import com.club_board.club_board_server.dto.myPage.UpdateMyPageRequest;
+import com.club_board.club_board_server.dto.myPage.userInfo.UserInfoResponse;
+import com.club_board.club_board_server.dto.myPage.userInfo.UpdateUserInfoRequest;
 import com.club_board.club_board_server.response.ResponseBody;
 import com.club_board.club_board_server.response.ResponseUtil;
 import com.club_board.club_board_server.service.myPage.MyPageService;
@@ -14,21 +14,22 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/myPage")
 public class MyPageController {
 
     private final MyPageService myPageService;
 
-    @GetMapping("/myPage/{userId}")
+    @GetMapping("/{userId}")
     @PreAuthorize("@tokenProvider.getUserIdFromToken(authentication.getCredentials(), #userId)")
-    public ResponseEntity<ResponseBody<MyPageResponse>> showMyPage(@PathVariable("userId") @P("userId") Long userId){
-        MyPageResponse myPageResponse=myPageService.getMyPage(userId);
-        return ResponseEntity.ok(ResponseUtil.createSuccessResponse(myPageResponse));
+    public ResponseEntity<ResponseBody<UserInfoResponse>> showMyPage(@PathVariable("userId") @P("userId") Long userId){
+        UserInfoResponse userInfoResponse =myPageService.getMyPage(userId);
+        return ResponseEntity.ok(ResponseUtil.createSuccessResponse(userInfoResponse));
     }
 
-    @PatchMapping("/myPage/{userId}")
+    @PatchMapping("/{userId}")
     @PreAuthorize("@tokenProvider.getUserIdFromToken(authentication.getCredentials(), #userId)")
-    public ResponseEntity<ResponseBody<String>> updateMyPage(@PathVariable("userId") @P("userId") Long userId, @RequestBody UpdateMyPageRequest updateMyPageRequest){
-        myPageService.updateMyPage(userId,updateMyPageRequest);
+    public ResponseEntity<ResponseBody<String>> updateMyPage(@PathVariable("userId") @P("userId") Long userId, @RequestBody UpdateUserInfoRequest updateUserInfoRequest){
+        myPageService.updateMyPage(userId, updateUserInfoRequest);
         return ResponseEntity.ok(ResponseUtil.createSuccessResponse("유저 정보 업데이트 성공"));
     }
 }
