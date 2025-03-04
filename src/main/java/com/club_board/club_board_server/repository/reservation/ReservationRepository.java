8332ed615aae947e9fe5058305c1db4debc30a66
Juhye0k <kyoung1678@naver.com>
2025-03-04 21:47:39 +0900

    Bug:findReservationByBookAndUserId JPQL 수정 #47

--- src/main/java/com/club_board/club_board_server/repository/reservation/ReservationRepository.java
+++ src/main/java/com/club_board/club_board_server/repository/reservation/ReservationRepository.java
@@ -26,7 +26,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Query("SELECT COUNT(r) FROM Reservation r WHERE r.book.id = :bookId AND (r.status='RESERVED' OR r.status='BORROWING')")
     int countActiveReservation(@Param("bookId") Long bookId);
 
-    @Query("SELECT r From Reservation r WHERE r.book.id=:bookId AND r.user.id=:userId")
+    @Query("SELECT r From Reservation r WHERE r.book.id=:bookId AND r.user.id=:userId AND (r.status='RESERVED' OR r.status='BORROWING')")
     Optional<Reservation> findReservationByBookAndUserId(@Param("bookId") Long bookId, @Param("userId") Long userId);
 
     @Query("select new com.club_board.club_board_server.dto.myPage.book.MyLoanResponse(" +
