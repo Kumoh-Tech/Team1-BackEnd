87577f89a0877db4f880a44270767da6391fd9a7
Juhye0k <kyoung1678@naver.com>
2025-03-06 17:43:41 +0900

    refactor: 책 상세조회 시 상태 값 전달 #49

--- src/main/java/com/club_board/club_board_server/service/book/BookService.java
+++ src/main/java/com/club_board/club_board_server/service/book/BookService.java
@@ -41,7 +41,7 @@ public class BookService {
                                 bookUrl=s3Service.generateBookImageDownloadUrl(book.getBookImage().getId()).getUrl();
                             }
                             int borrowCount=reservationRepository.countActiveReservation(book.getId());
-                            boolean isBorrowing = reservationRepository.findReservedReservationByBookAndUser(book.getId(), userId).isPresent();
+                            ReservationStatus userReservationStatus=checkUserBookStatus(book.getId(),userId);
 
                 return   BookResponse.builder()
                         .id(book.getId())
@@ -53,22 +53,22 @@ public class BookService {
                         .bookImageId(book.getBookImage().getId())
                         .bookUrl(bookUrl)
                         .borrowCount(borrowCount)
-                        .isBorrowingBook(isBorrowing)
+                        .reservationStatus(userReservationStatus)
                         .build();
                 })
                 .collect(Collectors.toList());
     }
 
     // 책 상세내역 조회
-    public BookResponse getBookById(Long id,Long userId){
-        Book book=bookRepository.findById(id)
+    public BookResponse getBookById(Long bookId,Long userId){
+        Book book=bookRepository.findById(bookId)
                 .orElseThrow(()->new BusinessException(ExceptionType.BOOK_NOT_FOUND));
         String bookUrl=null;
         if(book.getBookImage()!=null){
             bookUrl=s3Service.generateBookImageDownloadUrl(book.getBookImage().getId()).getUrl();
         }
         int borrowCount=reservationRepository.countActiveReservation(book.getId());
-        boolean isBorrowing = reservationRepository.findReservedReservationByBookAndUser(id, userId).isPresent();
+        ReservationStatus userReservationStatus=checkUserBookStatus(bookId,userId);
         return BookResponse.builder()
                 .id(book.getId())
                 .author(book.getAuthor())
@@ -79,7 +79,7 @@ public class BookService {
                 .bookImageId(book.getBookImage().getId())
                 .bookUrl(bookUrl)
                 .borrowCount(borrowCount)
-                .isBorrowingBook(isBorrowing)
+                .reservationStatus(userReservationStatus)
                 .build();
     }
 
@@ -147,5 +147,15 @@ public class BookService {
             reservation.getUser().setOverdue(true);
             reservationRepository.save(reservation);
         }
+    }
+    public ReservationStatus checkUserBookStatus(Long id,Long userId){
+        boolean isReserved = reservationRepository.findReservedReservationByBookAndUser(id, userId).isPresent();
+        boolean isBorrowed = reservationRepository.findReservedReservationByBookAndUser(id, userId).isPresent();
+        ReservationStatus userReservationStatus=null;
+        if(isReserved)
+            userReservationStatus=ReservationStatus.RESERVED;
+        if(isBorrowed)
+            userReservationStatus=ReservationStatus.BORROWING;
+        return userReservationStatus;
     }
 }
