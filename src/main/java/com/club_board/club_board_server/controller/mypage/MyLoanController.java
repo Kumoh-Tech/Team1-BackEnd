55688c53e58820ea036f95bf65c9fa92226f491c
Juhye0k <kyoung1678@naver.com>
2025-03-09 21:59:30 +0900

    refactor: 마이페이지 URI 수정 #52

--- src/main/java/com/club_board/club_board_server/controller/mypage/MyLoanController.java
+++ src/main/java/com/club_board/club_board_server/controller/mypage/MyLoanController.java
@@ -1,5 +1,6 @@
 package com.club_board.club_board_server.controller.mypage;
 
+import com.club_board.club_board_server.domain.user.CustomUserDetails;
 import com.club_board.club_board_server.dto.myPage.book.MyLoanResponse;
 import com.club_board.club_board_server.dto.myPage.book.MyReservationResponse;
 import com.club_board.club_board_server.dto.myPage.book.MyReturnResponse;
@@ -9,9 +10,8 @@ import com.club_board.club_board_server.service.myPage.MyLoanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.parameters.P;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,24 +23,27 @@ import java.util.List;
 public class MyLoanController {
 
     private final MyLoanService myLoanService;
-    @GetMapping("/loan/{userId}")
-    @PreAuthorize("@tokenProvider.getUserIdFromToken(authentication.getCredentials(), #userId)")
-    public ResponseEntity<ResponseBody<List<MyLoanResponse>>> getLoanStatus(@PathVariable("userId") @P("userId") Long userId){
+    @GetMapping("/loan")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER','ROLE_ADMIN','ROLE_OWNER')")
+    public ResponseEntity<ResponseBody<List<MyLoanResponse>>> getLoanStatus(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        Long userId=customUserDetails.getUser().getId();
         List<MyLoanResponse> myLoanResponse=myLoanService.getMyLoan(userId);
         return ResponseEntity.ok(ResponseUtil.createSuccessResponse(myLoanResponse));
     }
 
-    @GetMapping("/reservation/{userId}")
-    @PreAuthorize("@tokenProvider.getUserIdFromToken(authentication.getCredentials(), #userId)")
-    public ResponseEntity<ResponseBody<List<MyReservationResponse>>> getReservationStatus(@PathVariable("userId") @P("userId") Long userId){
+    @GetMapping("/reservation")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER','ROLE_ADMIN','ROLE_OWNER')")
+    public ResponseEntity<ResponseBody<List<MyReservationResponse>>> getReservationStatus(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        Long userId=customUserDetails.getUser().getId();
         List<MyReservationResponse> myReservationResponse=myLoanService.getMyReservations(userId);
         return ResponseEntity.ok(ResponseUtil.createSuccessResponse(myReservationResponse));
     }
 
-    @GetMapping("/return/{userId}")
-    @PreAuthorize("@tokenProvider.getUserIdFromToken(authentication.getCredentials(), #userId)")
-    public ResponseEntity<ResponseBody<List<MyReturnResponse>>> getReturnStatus(@PathVariable("userId") @P("userId") Long userId){
-        List<MyReturnResponse> myLoanResponse=myLoanService.getMyReturn(userId);
-        return ResponseEntity.ok(ResponseUtil.createSuccessResponse(myLoanResponse));
+    @GetMapping("/return")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER','ROLE_ADMIN','ROLE_OWNER')")
+    public ResponseEntity<ResponseBody<List<MyReturnResponse>>> getReturnStatus(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        Long userId=customUserDetails.getUser().getId();
+        List<MyReturnResponse> myReturnResponse=myLoanService.getMyReturn(userId);
+        return ResponseEntity.ok(ResponseUtil.createSuccessResponse(myReturnResponse));
     }
 }
