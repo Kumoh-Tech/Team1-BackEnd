d41f83d749db812f5e21f78857fb0d4cc44974dc
jungbk0808 <jungbk0808@gmail.com>
2025-04-02 10:29:20 +0900

    :bug:fix: 대출 중 데이터 조회 시 잘못된 데이터 수정

--- src/main/java/com/club_board/club_board_server/dto/myPage/book/MyLoanResponse.java
+++ src/main/java/com/club_board/club_board_server/dto/myPage/book/MyLoanResponse.java
@@ -1,14 +1,27 @@
 package com.club_board.club_board_server.dto.myPage.book;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import java.time.LocalDateTime;
 
+import static com.club_board.club_board_server.service.book.BookAdminService.MAX_LOAN_PERIOD_DAYS;
+
 @Getter
-@AllArgsConstructor
 public class MyLoanResponse {
     private Long bookId;
     private String bookName;
     private LocalDateTime loanDate;
     private LocalDateTime returnDate;
     private Integer overdueDate;
+
+    public MyLoanResponse(Long bookId, String bookName, LocalDateTime loanDate, Integer overdueDate) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+        this.loanDate = loanDate;
+        this.returnDate = loanDate.plusDays(MAX_LOAN_PERIOD_DAYS);
+        if (overdueDate < 0) {
+            this.overdueDate = 0;
+        } else {
+            this.overdueDate = overdueDate;
+        }
+    }
 }
