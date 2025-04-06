b66879c0a318aef3cc9d82c7f7ff39c2d4f16ca8
Juhye0k <kyoung1678@naver.com>
2025-04-06 22:03:42 +0900

    refactor: 쿠키 수정

--- src/main/java/com/club_board/club_board_server/config/jwt/TokenProvider.java
+++ src/main/java/com/club_board/club_board_server/config/jwt/TokenProvider.java
@@ -79,24 +79,20 @@ public class TokenProvider {
         } catch (ExpiredJwtException e) { // 토큰이 만료되었을 때
             if(tokenType==TokenType.ACCESS){
                 log.info("액세스 토큰 만료");
-                clearAccessTokenCookie(response);
                 throw new BusinessException(ExceptionType.EXPIRED_ACCESS_TOKEN);
             }
             else {
                 log.info("리프레시 토큰 만료");
-                clearRefreshTokenCookie(response);
                 throw new BusinessException(ExceptionType.EXPIRED_REFRESH_TOKEN);
             }
         }
         catch (Exception e) { //토큰이 유효하지 않을 때
             if(tokenType==TokenType.ACCESS){
                 log.info("액세스 토큰 유효하지 않음");
-                clearAccessTokenCookie(response);
                 throw new BusinessException(ExceptionType.INVALID_ACCESS_TOKEN);
             }
             else {
                 log.info("리프레시 토큰 유효하지 않음");
-                clearRefreshTokenCookie(response);
                 throw new BusinessException(ExceptionType.INVALID_REFRESH_TOKEN);
             }
         }
